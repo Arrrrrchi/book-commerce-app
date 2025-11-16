a8f7806d424e23466d430abdafe0fd5d62ef76c4
Arrrrrchi <esh6c4nm@gmail.com>
2025-11-16 11:34:34 +0900

    本をクリックした時のモーダルを追加

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Book as BookType } from "@/app/types/types";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 interface BookProps {
   book: BookType;
@@ -10,10 +13,33 @@ interface BookProps {
 
 // eslint-disable-next-line react/display-name
 const Book = ({ book }: BookProps) => {
+  const [showModal, setShowModal] = useState(false);
+  const { data: session } = useSession();
+  const user = session?.user;
+  const router = useRouter();
+
+  const handleParchaseClick = () => {
+    setShowModal(true);
+  }
+
+  const handlePachaseConfirm = () => {
+    if (!user) {
+      setShowModal(false);
+      // ログインページへリダイレクト
+      router.push('/logout');
+    } else {
+      // stripeで決済する
+    }
+  }
+
+  const handleCancel = () => {
+    setShowModal(false);
+  }
+
   return (
     <>
       {/* アニメーションスタイル */}
-      <style jsx global>{`
+      <style jsx>{`
         @keyframes fadeIn {
           from {
             opacity: 0;
@@ -24,13 +50,13 @@ const Book = ({ book }: BookProps) => {
             transform: scale(1);
           }
         }
-        .modal {
+        .modal-content {
           animation: fadeIn 0.3s ease-out forwards;
         }
       `}</style>
 
       <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a onClick={handleParchaseClick} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
             src={book.thumbnail.url}
@@ -46,17 +72,19 @@ const Book = ({ book }: BookProps) => {
           </div>
         </a>
 
-        {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
+        {showModal && (
+          <div className="fixed top-0 left-0 right-0 bottom-0 bg-slate-900/50 flex justify-center items-center z-50">
+            <div className="bg-white p-8 rounded-lg modal-content">
+              <h3 className="text-xl mb-4">本を購入しますか？</h3>
+              <button onClick={handlePachaseConfirm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+                購入する
+              </button>
+              <button onClick={handleCancel} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                キャンセル
+              </button>
+            </div>
           </div>
-        </div> */}
+        )}
       </div>
     </>
   );
